48e5e3f871dcdb5dc50a9c64603f9f9e547117b6
Mathtzt <matheus.cn10@gmail.com>
2020-08-26 11:10:15 -0300

    Finalizando estudo inicial de OO em Python

--- orientacao-objeto/src/conta.py
+++ orientacao-objeto/src/conta.py
@@ -1,16 +1,51 @@
 class Conta:
     ##construtor no python... manter o parametro self inserido pelo python que significa o endereço de memória que será gravado o objeto criado
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        ##Dois underscore a frente do atributo significa que ele é um atributo privado
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("Seu saldo atual é R$ {}.".format(self.saldo))
+        print("Seu saldo atual é R$ {}.".format(self.__saldo))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        if (self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("Valor superior ao limite disponível atualmente.")
+
+    def tranfere(self, valor, conta_destino):
+        self.saca(valor)
+        conta_destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular.capitalize()
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, valor):
+        self.__limite = valor
+
+    def __pode_sacar(self, valor):
+        return valor <= self.__saldo + self.__limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigo_banco_list():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
--- orientacao-objeto/src/testes.py
+++ orientacao-objeto/src/testes.py
@@ -1,14 +1,19 @@
-from conta import Conta
-from datas import Data
-
+from src.conta import Conta
+from src.datas import Data
 
 def test_class_conta():
     print("Testando classe conta...")
     conta = Conta(3512, "Exemplo", 500.0, 1500.0)
+    conta2 = Conta(1412, "Exemplo2", 250.0, 1500.0)
     conta.extrato()
     conta.deposita(150.0)
     conta.extrato()
-
+    conta.tranfere(250, conta2)
+    conta.extrato()
+    conta2.extrato()
+    conta.saca(2000)
+    print(Conta.codigo_banco())
+    print(Conta.codigo_banco_list())
 
 def test_class_data():
     print("Testando classe data...")
