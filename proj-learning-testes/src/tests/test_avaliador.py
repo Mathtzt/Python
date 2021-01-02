4977cb7964f2c7c0e063cf5e3cf20b2b9aca2254
Mathtzt <matheus.cn10@gmail.com>
2021-01-02 14:05:55 -0300

    Renomeando teste

--- proj-learning-testes/src/tests/test_avaliador.py
+++ proj-learning-testes/src/tests/test_avaliador.py
@@ -5,15 +5,20 @@ from src.utils.dominio_utils import DominioUtils
 
 
 class TestAvaliador(TestCase):
-    def test_avalia(self):
+    def test_deve_retornar_menor_e_maior_valor_leilao(self):
         leilao = DominioUtils.gera_lances_leilao()
 
         avaliador = Avaliador()
         avaliador.avalia(leilao)
 
         menor_valor_esperado = 100
+        maior_valor_esperado = 200
 
         self.assertEqual(
             menor_valor_esperado,
             avaliador.menor_lance
+        )
+        self.assertEqual(
+            maior_valor_esperado,
+            avaliador.maior_lance
         )
