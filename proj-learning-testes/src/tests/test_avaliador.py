6e1208e2e0a73bd6e4ca7f494ea66774cdce9663
Mathtzt <matheus.cn10@gmail.com>
2021-01-02 14:39:35 -0300

    Finalizando aula 2

--- proj-learning-testes/src/tests/test_avaliador.py
+++ proj-learning-testes/src/tests/test_avaliador.py
@@ -5,6 +5,21 @@ from src.utils.dominio_utils import DominioUtils
 
 
 class TestAvaliador(TestCase):
+    """
+    Outras dicas/métodos importantes da classe TestCase:
+    -> setUp: executa o trecho de código antes de cada método de testes
+    -> tearDown: executa o trecho de código logo após a execução de cada teste
+    -> setUpClass: executa o trecho de código no momento que a classe é criada
+    -> tearDownClass: executa o trecho de código após o último teste da classe ser rodado
+
+    Obs: Os métodos tearDown e tearDownClass são muito utilizados em testes que
+    integram duas partes do sistema - banco de dados, sistemas externos,
+    ou então desejamos fechar uma conexão que foi aberta.
+
+    Tais tipos de testes, que verificam como duas partes do sistema se integram,
+    são chamados de testes de integração.
+    """
+
     def test_deve_retornar_menor_e_maior_valor_leilao(self):
         leilao = DominioUtils.gera_lances_leilao()
 
@@ -20,5 +35,22 @@ class TestAvaliador(TestCase):
         )
         self.assertEqual(
             maior_valor_esperado,
+            avaliador.maior_lance
+        )
+
+    def test_deve_retornar_valores_iguais_quando_lance_unico(self):
+        leilao = DominioUtils.gera_lance_unico_leilao()
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        valor_lance_unico = 150
+
+        self.assertEqual(
+            valor_lance_unico,
+            avaliador.menor_lance
+        )
+        self.assertEqual(
+            valor_lance_unico,
             avaliador.maior_lance
         )
--- proj-learning-testes/src/utils/dominio_utils.py
+++ proj-learning-testes/src/utils/dominio_utils.py
@@ -7,11 +7,25 @@ class DominioUtils:
     def gera_lances_leilao():
         math = Usuario("Math")
         ana = Usuario("Ana")
+        joao = Usuario("Joao")
 
         lance_da_ana = Lance(ana, 100)
         lance_do_math = Lance(math, 200)
+        lance_do_joao = Lance(joao, 150)
 
         leilao = Leilao('Churrasqueira')
         leilao.lances.append(lance_da_ana)
         leilao.lances.append(lance_do_math)
+        leilao.lances.append(lance_do_joao)
         return leilao
+
+    @staticmethod
+    def gera_lance_unico_leilao():
+        fabio = Usuario("Fabio")
+        lance_do_fabio = Lance(fabio, 150)
+
+        leilao = Leilao('Celular')
+        leilao.lances.append(lance_do_fabio)
+
+        return leilao
+
